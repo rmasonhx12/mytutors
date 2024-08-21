3ab03a5e71e20073b1a57f49f1ebefda0ffee70c
Robert Hendricks <rmasonh22@gmail.com>
2024-08-21 01:16:23 -0400

    clean up

--- app/tutors/[id]/page.jsx
+++ app/tutors/[id]/page.jsx
@@ -1,11 +1,6 @@
-//'use client'
-//import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation'
+
 
 const TutorPage = ({ params }) => {
-  //const router = useRouter();
-  //const params = useParams();
-  //const searchParams = useSearchParams();
-  //const pathname = usePathname();
   return (
     <div>
       Tutor Page {params.id} 
