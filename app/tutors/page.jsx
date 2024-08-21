348152a59413b795907161edf4e6a0d827590271
Robert Hendricks <rmasonh22@gmail.com>
2024-08-21 00:46:29 -0400

    use client example

--- app/tutors/page.jsx
+++ app/tutors/page.jsx
@@ -1,10 +1,15 @@
 // Code: app/tutors/page.jsx copyright 2024 Bujisoft
+'use client';
+import { useRouter } from "next/navigation";
 //import Link from 'next/link';
 
 const TutorsPage = () => {
+  const router = useRouter();
+  console.log(router);
   return (
     <div>
       <h1>Tutors</h1>
+      <button onClick={ () => router.replace('/') }> Go Home</button>
     
     </div>
   );
